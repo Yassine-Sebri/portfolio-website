3f6a65d9b1fe2c69b94a9261102b3befb364e4fa
Yassine Sebri <ysebri@mantu.com>
2023-03-20 13:35:24 +0100

    add cypress for running tests

---
+++ cypress/e2e/Navigation.cy.js
@@ -0,0 +1,25 @@
+describe("Navigation", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+  it("should navigate to the Projects page", () => {
+    // Go to projects
+    cy.get('a[href*="projects"]').eq(0).click();
+    cy.url().should("include", "/projects");
+    cy.get("h1").contains("Projects");
+
+    // Go back to Home
+    cy.get('a[href="/"]').click();
+    cy.get("h1").contains("About Me");
+
+    // Go to Experience
+    cy.get('a[href*="experience"]').eq(0).click();
+    cy.url().should("include", "/experience");
+    cy.get("h1").contains("Experience");
+
+    // Go to Contact
+    cy.get('a[href*="contact"]').eq(0).click();
+    cy.url().should("include", "/contact");
+    cy.get("h1").contains("Contact");
+  });
+});
