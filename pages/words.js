9375ae843994f60352376366170c8c2290a212e8
Yassine Sebri <ysebri@mantu.com>
2023-03-26 00:43:18 +0100

    improve code

--- pages/words.js
+++ pages/words.js
@@ -27,21 +27,7 @@ export const getStaticProps = async () => {
 export default function words({ instances }) {
   return (
     <ContainerBlock title="Words - Yassine Sebri">
-      <section className="bg-white dark:bg-gray-800">
-        <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
-          <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
-            Words
-          </h1>
-        </div>
-        <div className="bg-[#F1F1F1] dark:bg-gray-900 -mt-4">
-          <div className="max-w-6xl mx-auto pt-16 px-10">
-            <div className="text-gray-500 ">
-              Leave a comment for future visitors.
-            </div>
-            <Comments instances={instances} />
-          </div>
-        </div>
-      </section>
+      <Comments instances={instances} />
     </ContainerBlock>
   );
 }
